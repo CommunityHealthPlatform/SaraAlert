3f9e37e62ed0041ed27bc667cf8522c306ffff4a
Becky Hack <rhack@mitre.org>
2021-05-07 23:22:50 -0400

    Minor refactoring of the FollowUpFlag component

--- app/javascript/components/patient/PatientPage.js
+++ app/javascript/components/patient/PatientPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { Card } from 'react-bootstrap';
+import { Button, Card, Modal } from 'react-bootstrap';
 import Patient from './Patient';
 import Dependent from './household/Dependent';
 import HeadOfHousehold from './household/HeadOfHousehold';
@@ -8,27 +8,34 @@ import Individual from './household/Individual';
 import FollowUpFlag from './FollowUpFlag';
 
 class PatientPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      action: undefined,
+    };
+  }
+
   render() {
     return (
       <React.Fragment>
         <Card id="patient-page" className="mx-2 card-square">
           <Card.Header className="h5" id="patient-info-header">
             Monitoree Details {this.props.patient.user_defined_id ? `(ID: ${this.props.patient.user_defined_id})` : ''}{' '}
-            <FollowUpFlag
-              patient={this.props.patient}
-              current_user={this.props.current_user}
-              jurisdiction_paths={this.props.jurisdiction_paths}
-              authenticity_token={this.props.authenticity_token}
-              follow_up_reasons={this.props.follow_up_reasons}
-              other_household_members={this.props.other_household_members}
-            />
+            <Button size="md" className="my-2 mr-2" aria-label="Flag for follow up button" onClick={() => this.setState({ action: 'Flag for Follow-Up' })}>
+              <i className="fas fa-flag"></i>
+              {this.props.patient.follow_up_reason && ' Edit'} Flag for Follow-up
+            </Button>
           </Card.Header>
           <Card.Body>
             <Patient
+              current_user={this.props.current_user}
               jurisdiction_path={this.props.jurisdiction_path}
+              jurisdiction_paths={this.props.jurisdiction_paths}
               details={{ ...this.props.patient, blocked_sms: this.props.blocked_sms }}
               collapse={this.props.can_modify_subject_status}
               edit_mode={false}
+              follow_up_reasons={this.props.follow_up_reasons}
+              other_household_members={this.props.other_household_members}
               authenticity_token={this.props.authenticity_token}
             />
             <div className="household-info">
@@ -53,6 +60,23 @@ class PatientPage extends React.Component {
             </div>
           </Card.Body>
         </Card>
+        <Modal size="lg" centered show={this.state.action !== undefined} onHide={() => this.setState({ action: undefined })}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.state.action}</Modal.Title>
+          </Modal.Header>
+          {this.state.action === 'Flag for Follow-Up' && (
+            <FollowUpFlag
+              patient={this.props.patient}
+              current_user={this.props.current_user}
+              jurisdiction_paths={this.props.jurisdiction_paths}
+              authenticity_token={this.props.authenticity_token}
+              follow_up_reasons={this.props.follow_up_reasons}
+              other_household_members={this.props.other_household_members}
+              close={() => this.setState({ action: undefined })}
+              clear_flag={false}
+            />
+          )}
+        </Modal>
       </React.Fragment>
     );
   }
